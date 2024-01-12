68bec9945639974513be11e4914bb413acacd394
eakyoungyou <eakyoungyu@gmail.com>
2024-01-12 11:47:43 -0800

    Add CurrencyRateViewModel and connect with View

---
+++ app/src/main/java/com/kong/transmart/viewmodels/CurrencyRateViewModel.kt
@@ -0,0 +1,28 @@
+package com.kong.transmart.viewmodels
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.kong.transmart.models.Currency
+import com.kong.transmart.models.ExchangeRate
+
+class CurrencyRateViewModel: ViewModel() {
+    private var _exchangeRate: MutableState<ExchangeRate> = mutableStateOf<ExchangeRate>(
+        ExchangeRate(
+        Currency("CAD", "Canada", "$"),
+        Currency("KRW", "Korea", "\u20A9"),
+        988.22
+    ))
+
+    fun getSourceCurrency(): Currency {
+        return _exchangeRate.value.from
+    }
+
+    fun getTargetCurrency(): Currency {
+        return _exchangeRate.value.to
+    }
+
+    fun getCurrencyRate(): Double {
+        return _exchangeRate.value.rate
+    }
+}
--- app/src/main/java/com/kong/transmart/views/MainView.kt
+++ app/src/main/java/com/kong/transmart/views/MainView.kt
@@ -30,6 +30,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kong.transmart.viewmodels.CurrencyRateViewModel
 
 @Composable
 fun MainView() {
@@ -38,34 +40,43 @@ fun MainView() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        CurrencyRateView()
+        val viewModel:CurrencyRateViewModel = viewModel()
+        CurrencyRateView(viewModel)
         Spacer(modifier = Modifier.padding(bottom = 16.dp))
-        CurrencyCalculateView()
+        CurrencyCalculateView(viewModel)
     }
 }
 
 @Composable
-fun CurrencyRateView() {
+fun CurrencyRateView(viewModel: CurrencyRateViewModel) {
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val sourceCode = viewModel.getSourceCurrency().code
+        val targetCode = viewModel.getTargetCurrency().code
+        val currencyRate = viewModel.getCurrencyRate()
         Text(
-            text = "1 CAD = 998.89 KRW",
+            text = "1 $sourceCode = $currencyRate $targetCode",
             style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold)
         )
     }
 }
 
 @Composable
-fun CurrencyCalculateView() {
-    var targetAmount by remember {
+fun CurrencyCalculateView(viewModel: CurrencyRateViewModel) {
+    val sourceCurrency = viewModel.getSourceCurrency()
+    val targetCurrency = viewModel.getTargetCurrency()
+    val currencyRate = viewModel.getCurrencyRate()
+
+    var sourceAmount by remember {
         mutableStateOf("100")
     }
-    var bestDstAmount by remember {
-        mutableStateOf("100000")
+    var bestTargetAmount: Double by remember {
+        mutableStateOf(sourceAmount.toDouble() * currencyRate)
     }
+
 
     Column(
         modifier = Modifier.fillMaxWidth(),
@@ -81,14 +92,19 @@ fun CurrencyCalculateView() {
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(text = "CAD", style = MaterialTheme.typography.bodyLarge)
+                Text(text = sourceCurrency.code, style = MaterialTheme.typography.bodyLarge)
 
                 BasicTextField(
-                    value = TextFieldValue(text = "$ $targetAmount", selection = TextRange("$ $targetAmount".length)),
+                    value = TextFieldValue(
+                        text = "${sourceCurrency.symbol} $sourceAmount",
+                        selection = TextRange("${sourceCurrency.symbol} $sourceAmount".length)
+                    ),
                     onValueChange = {
                         val newTarget = it.text.drop(2)
-                        if (newTarget.isDigitsOnly())
-                            targetAmount = newTarget
+                        if (newTarget.isDigitsOnly()) {
+                            sourceAmount = newTarget
+                            bestTargetAmount = currencyRate * (sourceAmount.toDoubleOrNull() ?: 0.0)
+                        }
                     },
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     textStyle = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.End),
@@ -109,9 +125,9 @@ fun CurrencyCalculateView() {
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(text = "KRW", style = MaterialTheme.typography.bodyLarge)
+                Text(text = targetCurrency.code, style = MaterialTheme.typography.bodyLarge)
                 Text(
-                    text = "\u20A9 " + String.format("%,d", bestDstAmount.toInt()),
+                    text = "${targetCurrency.symbol} " + String.format("%,.2f", bestTargetAmount),
                     style = MaterialTheme.typography.bodyLarge.copy(textAlign = TextAlign.End),
                 )
             }
