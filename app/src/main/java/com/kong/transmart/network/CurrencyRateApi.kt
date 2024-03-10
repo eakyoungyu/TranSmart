a8f81428c53582743ca754fa2fa750a955777d7b
eakyoungyou <eakyoungyu@gmail.com>
2024-03-10 13:54:52 -0700

    Set default parameters in CurrencyRateApi

--- app/src/main/java/com/kong/transmart/network/CurrencyRateApi.kt
+++ app/src/main/java/com/kong/transmart/network/CurrencyRateApi.kt
@@ -1,16 +1,15 @@
 package com.kong.transmart.network
 
+import com.kong.transmart.BuildConfig
 import com.kong.transmart.models.ExchangeRateResponse
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CurrencyRateApi {
     @GET("exchangeJSON")
     suspend fun getExchangeRate(
-        @Query("authkey") authkey: String,
+        @Query("authkey") authkey: String = BuildConfig.API_KEY,
         @Query("searchdate") searchdate: String,
-        @Query("data") data: String
+        @Query("data") data: String = "AP01"
     ): List<ExchangeRateResponse>?
 }
