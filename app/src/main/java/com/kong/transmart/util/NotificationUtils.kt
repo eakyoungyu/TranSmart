fca6f9690b5aa91a4487011b4320d8ebffd713fb
eakyoungyou <eakyoungyu@gmail.com>
2024-03-23 15:44:48 -0700

    Add sendNotification in NotificationUtils

--- app/src/main/java/com/kong/transmart/util/NotificationUtils.kt
+++ app/src/main/java/com/kong/transmart/util/NotificationUtils.kt
@@ -1,20 +1,73 @@
 package com.kong.transmart.util
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import com.kong.transmart.MainActivity
+import com.kong.transmart.R
+import java.util.concurrent.atomic.AtomicInteger
 
 object NotificationUtils {
-    fun sendNotification() {
-        // Send a notification
-    }
-
+    private val TAG = NotificationUtils::class.simpleName
+    private const val CHANNEL_ID = "com.kong.transmart"
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     fun hasPermission(context: Context): Boolean {
         return ContextCompat.checkSelfPermission(context,
             Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel(CHANNEL_ID, "Transmart", importance)
+            mChannel.description = "Exchange rate notification channel"
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+
+    fun sendNotification(context: Context, title: String, content: String) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        var builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.transmart_notification_large)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setColor(ContextCompat.getColor(context, R.color.dark_gray))
+
+        with(NotificationManagerCompat.from(context)) {
+            // notificationId is a unique int for each notification that you must define.
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.e(TAG, "No permission to post notification")
+                return
+            }
+            notify(NotificationID.id, builder.build())
+        }
+    }
+
+    object NotificationID {
+        val id: Int
+            get() = AtomicInteger(0).incrementAndGet()
     }
 }
